34f0c80b6fe9f13ce41f9700c73aece930f98598
Nik Jigalov <njigalov@qualogy.com>
2017-06-22 14:56:24 +0200

    Ch02/Concepts - Design Principles 1. Practicing instantiating singletons.

--- OCP_Ch_02/DesignPrinciples.java
+++ OCP_Ch_02/DesignPrinciples.java
@@ -101,6 +101,53 @@ class SecretSubmarine extends NuclearSubmarine implements SpySubmarine {
 	}
 }
 
+// Singleton example
+class HayStorage {
+	private int quantity = 0;
+	private HayStorage() {}
+
+	private static final HayStorage instance = new HayStorage();
+
+	public static HayStorage getInstance() {
+		return instance;
+	}
+
+	public synchronized void addHay(int amount) {
+		this.quantity += amount;
+	}
+
+	public synchronized boolean removeHay(int amount) {
+		if (quantity < amount) return false;
+
+		quantity -= amount;
+
+		return true;
+	}
+
+	public synchronized int getHayQuantity() {
+		return this.quantity;
+	}
+}
+
+// Lazyload singleton
+class LazyHayStorage {
+	private static volatile LazyHayStorage instance;
+
+	private LazyHayStorage() {}
+
+	public static LazyHayStorage getInstance() {
+		if (instance == null) {
+			synchronized(LazyHayStorage.class) {
+				if (instance == null) {
+					instance = new LazyHayStorage();
+				}
+			}
+		}
+
+		return instance;
+	}
+}
+
 public class DesignPrinciples {
 	public static void main(String[] args) {
 		Submarine sub = new Submarine("Sub-001");
@@ -114,6 +161,10 @@ public class DesignPrinciples {
 		System.out.println(spySub);
 		System.out.println(spySub.spy());
 
+		//-------------------------------------------------------------------------
 
+		HayStorage hayStorageManager = HayStorage.getInstance();
+
+		LazyHayStorage lhs = LazyHayStorage.getInstance();
 	}
 }
