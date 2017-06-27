1b4cd428c8f85b3fb2e34578393b699e5a47b50c
Nik Jigalov <njigalov@qualogy.com>
2017-06-27 14:03:18 +0200

    Ch03/Concepts - Generics 1. Replicating compilor warning.

---
+++ OCP_Ch_03/Generics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Dragon {}
+
+class Unicorn {}
+
+public class Generics {
+	public static void main(String[] args) {
+		List unicorns = new ArrayList();
+		unicorns.add(new Unicorn());
+		printDragons(unicorns);
+	}
+
+	private static void printDragons(List<Dragon> dragons) {
+		for (Dragon dragon: dragons) {
+			System.out.println(dragon);
+		}
+	}
+}
