12ebcf33003fd6217ecdea68fd28a4bf0396dfa5
Nik Jigalov <njigalov@qualogy.com>
2017-06-28 10:30:45 +0200

    Ch03/Concepts - Generics

--- OCP_Ch_03/Generics.java
+++ OCP_Ch_03/Generics.java
@@ -6,9 +6,19 @@ class Unicorn {}
 
 public class Generics {
 	public static void main(String[] args) {
-		List unicorns = new ArrayList();
+		List unicorns = new ArrayList(); 	// Compiler warning: raw type
 		unicorns.add(new Unicorn());
 		printDragons(unicorns);
+
+		addUnicorn(unicorns);
+
+		try {
+			Unicorn unicorn = unicorns.get(0);
+		} catch (ClassCastException e) {
+			System.out.println("ClassCastException"); // 
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	private static void printDragons(List<Dragon> dragons) {
@@ -16,4 +26,9 @@ public class Generics {
 			System.out.println(dragon);
 		}
 	}
+
+	private static void addUnicorn(List unicorn) {
+		unicorn.add(new Dragon()); 			// Compiler warning: unchecked call to raw list type. 
+	}
 }
+
