03b1b682ef184640abe6d4bd8114c7828a6bf4d5
Nik Jigalov <njigalov@qualogy.com>
2017-08-04 15:29:02 +0200

    Ch07/Demo - Implementing comparisson 1. Comparing standard list generation and manipulation to stream generation and manipulation.

---
+++ OCP_Ch_07/demo/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+//        List<String> sample = new ArrayList<>();
+//
+//        for (int i = 0; i < 5000000; i++) {
+//          sample.add("element");
+//        }
+
+        // 90397ms
+//        for (String str : sample) {
+//            System.out.println(str.toUpperCase());
+//        }
+
+
+
+
+        Stream<String> sample3 = Stream.generate(() -> "element")
+                                        .limit(5000000);
+
+        // 94822ms
+//        sample3.forEach(str -> System.out.println(str.toUpperCase()));
+
+        // 124735ms
+        sample3.parallel()
+                .forEach(str -> System.out.println(str.toUpperCase()));
+
+        // ============================================================================================================
+//
+//        // 358ms
+//        for (String str : sample) {
+//            System.out.println(str.toUpperCase());
+//        }
+//
+//        // 809ms
+//        sample.stream()
+//                .forEach(s -> System.out.println(s.toUpperCase()));
+//
+//
+//        // 798ms
+//        sample.stream()
+//                .parallel()
+//                .forEach(s -> System.out.println(s.toUpperCase()));
+
+//        Stream<String> sample2 = Stream.of("hi","bye","what","no", "who");
+//        System.currentTimeMillis()
+//        sample2.forEach(s -> System.out.println(s.toUpperCase()));
+    }
+}
---
+++ OCP_Ch_07/questions/MemoryConsistancy.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class MemoryConsistancy {
+    public static void main(String[] args) {
+        try {
+            Map<String, Object> foodData = new HashMap<String, Object>();
+
+            foodData.put("penguin", 1);
+            foodData.put("flamingo", 2);
+
+            for (String key : foodData.keySet()) {
+                foodData.remove(key);
+            }
+        } catch (ConcurrentModificationException e) {
+            
+        }
+    }
+}
