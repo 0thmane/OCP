6868f37af2f2eb5fd7c36c02c1e6889c5f4d1584
Nik Jigalov <njigalov@qualogy.com>
2017-08-07 12:52:22 +0200

    Ch07/Demo - Skeleton 1. Moving ParallelStreams performance experimentation to experments folder. 2. Creating base classes and methods for demo.

---
+++ OCP_Ch_07/demo/MatrixManipulator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class MatrixManipulator {
+    private MatrixManipulator() {}
+
+    public static List<List<Integer>> addMatrices(List<List<Integer>> matrixA, List<List<Integer>> matrixB) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+}
---
+++ OCP_Ch_07/demo/ParallelMatrixManipulator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ParallelMatrixManipulator {
+    private ParallelMatrixManipulator() {}
+
+    public static List<List<Integer>> addMatrices(List<List<Integer>> matrixA, List<List<Integer>> matrixB) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+}
