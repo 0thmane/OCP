411043b04b03254a3e8f5d7071163161dbb90c7a
Nik Jigalov <njigalov@qualogy.com>
2017-08-08 13:16:54 +0200

    Ch07/Demo - Implemented ParrallelMatrixManipulator 1. Needed a way to index List<List<Integer>>, so used IntStream with size limits.

--- OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
+++ OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
@@ -8,6 +8,22 @@ public class ParallelMatrixManipulator {
     public static List<List<Integer>> addMatrices(List<List<Integer>> matrixA, List<List<Integer>> matrixB) {
         List<List<Integer>> result = new ArrayList<>();
         int rowSize = matrixA.get(0).size();
+        int columnSize = matrixA.size();
+
+        IntStream.range(0, columnSize)
+            .parallel()
+            .forEach(columnIndex -> {
+                List<Integer> resultingRow = new ArrayList<>();
+
+                IntStream.range(0, rowSize)
+                    .forEach(rowIndex -> {
+                        Integer newElement = new Integer(matrixA.get(columnIndex).get(rowIndex) + matrixB.get(columnIndex).get(rowIndex));
+
+                        resultingRow.add(newElement);
+                    });
+
+                result.add(resultingRow);
+            });
 
         // -------------------------------------------------------------------------------------------------------------------------------
         // Attempt 2
