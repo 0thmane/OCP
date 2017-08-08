b55f576548e4483f1b4679a2dbc0ec97f1e48c41
Nik Jigalov <njigalov@qualogy.com>
2017-08-08 13:17:47 +0200

    Ch07/Demo - Cleaning Code 1. Removing previous two attempts at implementing ParallelMatrixManipulator.

--- OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
+++ OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
@@ -25,35 +25,6 @@ public class ParallelMatrixManipulator {
                 result.add(resultingRow);
             });
 
-        // -------------------------------------------------------------------------------------------------------------------------------
-        // Attempt 2
-        /*
-        Stream.generate(() -> new ArrayList<Integer>())
-            .limit(matrixA.size())
-            .parallel()
-            .forEach(resultingRow -> {
-                IntStream.range(0, rowSize)
-                    .forEach(index -> {
-                        Integer newElement = new Integer(index);
-                        resultingRow.add(newElement);
-                    });
-
-                System.out.println(resultingRow);
-            });
-        */
-
-
-        // -------------------------------------------------------------------------------------------------------------------------------
-        // Attempt 1
-        /*
-        Stream.generate(() -> new ArrayList<Integer>())
-            .limit(matrixA.size())
-            .forEach(resultingRow -> {
-                Stream.generate(() -> new Integer(5))
-                    .limit(matrixA.get(0).size())
-                    .forEach(resultingElement -> System.out.println(resultingElement));
-            });
-        */
         return result;
     }
 }
