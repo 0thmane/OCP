ef78d70111d6a7c26f767063d8f6746563d47de1
Nik Jigalov <njigalov@qualogy.com>
2017-08-08 12:56:07 +0200

    Ch07/Demo - Trying to implement ParallelMatrixManipulator

--- OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
+++ OCP_Ch_07/demo/MatrixDemo/ParallelMatrixManipulator.java
@@ -1,11 +1,43 @@
 import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class ParallelMatrixManipulator {
     private ParallelMatrixManipulator() {}
 
     public static List<List<Integer>> addMatrices(List<List<Integer>> matrixA, List<List<Integer>> matrixB) {
         List<List<Integer>> result = new ArrayList<>();
+        int rowSize = matrixA.get(0).size();
 
+        // -------------------------------------------------------------------------------------------------------------------------------
+        // Attempt 2
+        /*
+        Stream.generate(() -> new ArrayList<Integer>())
+            .limit(matrixA.size())
+            .parallel()
+            .forEach(resultingRow -> {
+                IntStream.range(0, rowSize)
+                    .forEach(index -> {
+                        Integer newElement = new Integer(index);
+                        resultingRow.add(newElement);
+                    });
+
+                System.out.println(resultingRow);
+            });
+        */
+
+
+        // -------------------------------------------------------------------------------------------------------------------------------
+        // Attempt 1
+        /*
+        Stream.generate(() -> new ArrayList<Integer>())
+            .limit(matrixA.size())
+            .forEach(resultingRow -> {
+                Stream.generate(() -> new Integer(5))
+                    .limit(matrixA.get(0).size())
+                    .forEach(resultingElement -> System.out.println(resultingElement));
+            });
+        */
         return result;
     }
 }
