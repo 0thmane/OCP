3d04974c70b5260fe2be8766396170e07fbfb18b
Nik Jigalov <njigalov@qualogy.com>
2017-08-30 11:49:32 +0200

    Ch08 - File Class Example

---
+++ OCP_Ch_08/FileExample.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class FileExample {
+  public static void main(String[] args) {
+    File file = new File("demo");
+    System.out.println("File Exists: " + file.exists());
+
+    if(file.exists()) {
+      System.out.println("Absolute Path: " + file.getAbsolutePath());
+      System.out.println("Is Directory: " + file.isDirectory());
+      System.out.println("Parent Path: " + file.getParent());
+
+      if(file.isFile()) {
+        System.out.println("File size: " + file.length());
+        System.out.println("File LastModified: " + file.lastModified());
+      } else {
+        for(File subfile: file.listFiles()) {
+          System.out.println("\t-> " + subfile.getName());
+        }
+      }
+    }
+  }
+}
