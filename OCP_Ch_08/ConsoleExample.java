530f96fc93f642ee098fa6e372c64a0d6e5a7aa6
Nik Jigalov <njigalov@qualogy.com>
2017-09-06 15:39:13 +0200

    Ch08 - Expanding ConsoleExample 1. Refactored user & pass echo 2. Created resetPassword method that securely validates new password.

--- OCP_Ch_08/ConsoleExample.java
+++ OCP_Ch_08/ConsoleExample.java
@@ -1,16 +1,52 @@
+import java.util.Arrays;
+
 import java.io.Console;
+import java.io.IOException;
 
 public class ConsoleExample {
   private static final String PROMPT = "#:";
 
-  public static void main(String[] args) {
-    Console console = System.console();
+  private static Console console = System.console();
 
+  private static void userInfoEcho() {
     if (console != null) {
       String username = console.readLine("Username: ");
       char[] password = console.readPassword("Password: ");
 
-      console.writer().println ("You entered the following credentials: " + username + ":" + String.valueOf(password));
+      console.writer().println("You entered the following credentials: " + username + ":" + String.valueOf(password));
     }
+  }
+
+  private static boolean resetPassword() throws NumberFormatException, IOException {
+    boolean newPasswordValid = false;
+
+    char[] oldPassword = console.readPassword("Old password: ");
+    char[] newPassword = console.readPassword("New password: ");
+    char[] newPasswordConfirmed = console.readPassword("New password again: ");
+
+    newPasswordValid = Arrays.equals(newPassword, newPasswordConfirmed);
+
+    secureSensativeArrays(oldPassword, newPassword, newPasswordConfirmed);
+
+    console.flush(); // Ensure there's nothing left in the buffer
+
+    return newPasswordValid;
+  }
+
+  private static void secureSensativeArrays(char[]... sensativeArrays) {
+    for (char[] sensativeArray : sensativeArrays) {
+      if (sensativeArray != null) {
+        for (int i = 0; i < sensativeArray.length; i++) {
+          sensativeArray[i] = 'x';
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    try {
+      String comment = resetPassword() ? "\nPassword updated.\n" : "\nNew password missmatch.\n";
+      console.writer().println(comment);
+    } catch(IOException e) {}
   }
 }
