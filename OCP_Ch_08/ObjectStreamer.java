1bf2de32a1e34f1a1005280762475b9b0d3d0342
Nik Jigalov <njigalov@qualogy.com>
2017-09-06 14:06:00 +0200

    Ch08 - Further Object streamer experimentation

--- OCP_Ch_08/ObjectStreamer.java
+++ OCP_Ch_08/ObjectStreamer.java
@@ -1,6 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
 
+import java.io.Serializable;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,8 +10,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
-import java.io.Serializable;
 
 import java.io.IOException;
 import java.io.EOFException;
@@ -25,8 +25,9 @@ public class ObjectStreamer {
     try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(dataFile)))) {
       while(true) {
         Object object = in.readObject();
-        if(object instanceof Animal)
-          animals.add((Animal)object);
+        if(object instanceof Animal) {
+          animals.add((Animal) object);
+        }
       }
     } catch (EOFException e) {
       // File end reached
@@ -37,18 +38,21 @@ public class ObjectStreamer {
 
   public static void createAnimalsFile(List<Animal> animals, File dataFile) throws IOException {
     try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(dataFile)))) {
-      for(Animal animal: animals)
+      for(Animal animal: animals) {
         out.writeObject(animal);
+      }
     }
   }
 
   public static void main(String[] args) throws IOException, ClassNotFoundException {
     List<Animal> animals = new ArrayList<Animal>();
-    animals.add(new Animal("Tommy Tiger",5,'T'));
-    animals.add(new Animal("Peter Penguin",8,'P'));
+    animals.add(new Animal("Tommy Tiger", 5, 'T'));
+    animals.add(new Animal("Peter Penguin", 8, 'P'));
+    animals.add(new Animal("Peter Penguin", 8, 'Z'));
+    animals.add(new Animal());
 
     File dataFile = new File("data/animal.data");
-    createAnimalsFile(animals,dataFile);
+    createAnimalsFile(animals, dataFile);
 
     System.out.println(getAnimals(dataFile));
   }
@@ -59,11 +63,18 @@ public class ObjectStreamer {
  */
 class Animal implements Serializable {
   private static final long serialVersionUID = 2L;
-  private transient String name;
-  private transient int age = 10;
-  private static char type = 'C';
 
-  {this.age = 14;}
+  private String name;  // Will store values as expected
+  private int age = 10; // Will store values as expected
+
+  // private transient String name;  // skipped over during serialization, defaults to null
+  // private transient int age = 10; // skipped over during serialization, defaults to 0
+
+  private static char type = 'C'; // static, so takes last set value
+
+  {
+    this.age = 14;
+  }
 
   public Animal() {
     this.name = "Unknown";
