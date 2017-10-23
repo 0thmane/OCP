2eb621fbe8547de2acf51a9cb622b161d0c7f99f
Nik Jigalov <njigalov@qualogy.com>
2017-10-23 08:26:19 +0200

    Ch07/Demo - Finishing Refactoring 1. Completed Matrix class by adding addition and concurrent addition. 2. Removed obsolete classes. 3. Refactored demo's main class.

--- OCP_Ch_07/demo/MatrixDemo/src/Main.java
+++ OCP_Ch_07/demo/MatrixDemo/src/Main.java
@@ -7,100 +7,55 @@ import java.util.*;
  **/
 public class Main {
 
-  private static List<List<Integer>> matrixA = generateMatrixOne();
-  private static List<List<Integer>> matrixB = generateMatrixTwo();
-  private static List<List<Integer>> result = new ArrayList<>();
+  private static final int ITERATION_LIMIT = 1_000;
+  private static final int SQUARE_MATRIX_SIZE = 2;
 
-  private static void printMatrix(List<List<Integer>> matrix) {
-    StringBuilder strRepresentation = new StringBuilder();
-
-    matrix.forEach(column -> {
-              column.forEach(
-                      element -> strRepresentation.append(String.format("%5s", element)));
-              strRepresentation.append("\n");
-            }
-    );
-
-    System.out.println(strRepresentation);
-  }
-
-  private static void printTimeTaken(long startTime, long endTime) {
+  private void printTimeTaken(long startTime, long endTime) {
     long timeTaken = endTime - startTime;
 
     System.out.println("The calculation took " + timeTaken + "ms.");
   }
 
-  private static List<List<Integer>> generateMatrixOne() {
-    List<List<Integer>> matrix = new ArrayList<>();
-    List<Integer> row1 = Arrays.asList(1, 2, 3);
-    List<Integer> row2 = Arrays.asList(4, 5, 6);
-    List<Integer> row3 = Arrays.asList(7, 8, 9);
-
-    matrix.add(row1);
-    matrix.add(row2);
-    matrix.add(row3);
-
-    return matrix;
-  }
-
-  private static List<List<Integer>> generateMatrixTwo() {
-    List<List<Integer>> matrix = new ArrayList<>();
-    List<Integer> row1 = Arrays.asList(9, 8, 7);
-    List<Integer> row2 = Arrays.asList(6, 15, 4);
-    List<Integer> row3 = Arrays.asList(3, 2, 1);
-
-    matrix.add(row1);
-    matrix.add(row2);
-    matrix.add(row3);
-
-    return matrix;
-  }
-
-  private static void timeAddition(int iterationLimit, String methodology) {
+  private void timeAddition(String methodology, Matrix matrixA, Matrix matrixB) {
+    Matrix result = null;
     long startTime = System.currentTimeMillis();
 
-    for (int iteration = 0; iteration < iterationLimit; iteration++) {
+    for (int iteration = 0; iteration < ITERATION_LIMIT; iteration++) {
       switch (methodology) {
         case "Sequentially":
-          result = new MatrixManipulator().addMatrices(matrixA, matrixB);
+          result = matrixA.add(matrixB);
           break;
         case "Concurrently":
-          result = new ParallelMatrixManipulator().addMatrices(matrixA, matrixB);
+          result = matrixA.addConcurrently(matrixB);
           break;
       }
     }
 
     long endTime = System.currentTimeMillis();
 
-    System.out.print(methodology + " " + iterationLimit + " times: ");
+    System.out.print(methodology + " " + ITERATION_LIMIT + " times: ");
     printTimeTaken(startTime, endTime);
-    printMatrix(result);
+    System.out.println(result);
+  }
+
+  public void runDemo() {
+    Matrix matrixA = Matrix.randomSquareMatrix(SQUARE_MATRIX_SIZE);
+    Matrix matrixB = Matrix.randomSquareMatrix(SQUARE_MATRIX_SIZE);
+
+    printMatrixOperation(matrixA, matrixB);
+
+    this.timeAddition("Sequentially", matrixA, matrixB);
+    this.timeAddition("Concurrently", matrixA, matrixB);
+  }
+
+  private void printMatrixOperation(Matrix matrixA, Matrix matrixB) {
+    System.out.println(matrixA);
+    System.out.println("+");
+    System.out.println(matrixB);
+    System.out.println("=");
   }
 
   public static void main(String[] args) {
-//        int iterationLimit = 1_000;
-//
-//        // ---- Printing the Matrices
-//        printMatrix(matrixA);
-//        System.out.println("+");
-//        printMatrix(matrixB);
-//        System.out.println("=");
-//
-//        // ---- Sequentially adding the Matrices
-//        timeAddition(iterationLimit, "Sequentially");
-//
-//        // ---- Parallel adding of the Matrices
-//        timeAddition(iterationLimit, "Concurrently");
-
-    // --------------------------------------------------------------------------------------------------------
-    int iterationLimit = 1_000;
-    int matrixSize = 4;
-
-    Matrix matrixA = Matrix.randomMatrix(matrixSize, 2);
-    Matrix matrixB = Matrix.randomSquareMatrix(matrixSize);
-
-    System.out.println(matrixA);
-//    System.out.println(matrixB);
-    System.out.println(matrixA.getSize());
+    new Main().runDemo();
   }
 }
--- OCP_Ch_07/demo/MatrixDemo/src/Matrix.java
+++ OCP_Ch_07/demo/MatrixDemo/src/Matrix.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 // 2D matrix
@@ -47,14 +51,59 @@ public class Matrix {
     return matrix.size() + "x" + matrix.get(0).size();
   }
 
-  // TODO: Add Matrix
-  public Matrix add(Matrix other) {
-    return this;
+  public int getHeight() {
+    return matrix.size();
   }
 
-  // TODO: ConcurrentAddMatrix
+  public int getWidth() {
+    return matrix.get(0).size();
+  }
+
+  public List<List<Integer>> getRawRepresentation() {
+    return this.matrix;
+  }
+
+  public Matrix add(Matrix other) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<List<Integer>> otherRawMatrix = other.getRawRepresentation();
+
+    IntStream.range(0, other.getWidth())
+      .forEach(index -> {
+        List<Integer> matrixAColumn = this.matrix.get(index);
+        List<Integer> matrixBColumn = otherRawMatrix.get(index);
+
+        List<Integer> resultingRow = addMatrixColumns(matrixAColumn, matrixBColumn);
+        result.add(resultingRow);
+      });
+
+    return Matrix.of(result);
+  }
+
   public Matrix addConcurrently(Matrix other) {
-    return this;
+    List<List<Integer>> result = new ArrayList<>();
+    List<List<Integer>> otherRawMatrix = other.getRawRepresentation();
+
+    int matrixSize = other.getWidth();
+
+    ExecutorService executor = Executors.newFixedThreadPool(3);
+
+    IntStream.range(0, matrixSize)
+      .forEach(index -> {
+        List<Integer> matrixAColumn = this.matrix.get(index);
+        List<Integer> matrixBColumn = otherRawMatrix.get(index);
+
+        Future<List<Integer>> futureRow = executor.submit(() -> addMatrixColumns(matrixAColumn, matrixBColumn));
+
+        try {
+          result.add(futureRow.get());
+        } catch (InterruptedException | ExecutionException e) {
+          e.printStackTrace();
+        }
+      });
+
+    executor.shutdown();
+
+    return Matrix.of(result);
   }
 
   @Override
@@ -86,6 +135,4 @@ public class Matrix {
 
     return resultingRow;
   }
-
-  // TODO: Private compare size
 }
